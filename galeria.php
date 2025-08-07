300c88579ac5fe1823db5fdd14ad3149370bfffd
GuiOP11 <deoliveirapadilhag@gmail.com>
2025-08-07 16:57:12 -0300

    gui

---
+++ galeria.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Galeria</h3>
+    <?php
+    require_once 'src/ClienteDAO.php';
+    $clientes = ClienteDAO::listar();
+    ?>
+</body>
+</html>
